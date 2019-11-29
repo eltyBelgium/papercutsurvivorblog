a6a379ba11e8bfa30070f3e257e82ad34b9e1e89
Tyas Lambrechts <tyas.lambrechts@triple-a-it.be>
2019-11-29 09:20:01 +0100

    setting startup paralax

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css' }
     ]
   },
   /*
@@ -28,13 +30,15 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+
   ],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
   ],
   /*
   ** Nuxt.js modules
@@ -58,5 +62,8 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
+  scripts: [
+
+  ]
 }
